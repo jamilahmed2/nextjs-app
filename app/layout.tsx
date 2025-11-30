409b4c427a0a2a33c1b2260c542528326c90e5ea
jamilahmed2 <ja0935991@gmail.com>
2025-11-30 21:55:03 +0500

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        Main Navbar
         {children}
+        Main Footer
       </body>
     </html>
   );
